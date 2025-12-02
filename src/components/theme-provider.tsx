ca4f563254fe8baf7f044fd4eca58e57220d6ca5
Haris Bhora <bhoraniyaharis963@gmail.com>
2025-12-02 13:24:49 +0530

    fix theme provider types

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,10 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+// yaha se type nikal rahe hain, extra import path ki zaroorat nahi
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+
+export function ThemeProvider(props: ThemeProviderProps) {
+  return <NextThemesProvider {...props} />;
 }
